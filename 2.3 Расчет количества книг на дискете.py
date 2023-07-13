3f62fe950c333861f60d0439e889144dfcb56d3b
egrmxmv <134498092+egrmxmv@users.noreply.github.com>
2023-07-13 17:42:12 +0300

    Лабораторная 1 (исправления)

--- 2.3 Расчет количества книг на дискете.py
+++ 2.3 Расчет количества книг на дискете.py
@@ -1,12 +1,13 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-space_disk_in_Mbytes = 1.44
-Bytes_in_Kbytes = 1024
-Kbytes_in_Mbytes = 1024
-space_disk_in_Bytes = (space_disk_in_Mbytes * Kbytes_in_Mbytes) * Bytes_in_Kbytes
+space_disk_in_mbytes = 1.44
+BYTES_IN_KBYTES = 1024
+KBYTES_IN_MBYTES = 1024
+space_disk_in_bytes = (space_disk_in_mbytes * KBYTES_IN_MBYTES) * BYTES_IN_KBYTES
 pages_in_book = 100
 lines_in_book = 50
 symbols_in_line = 25
-Bytes_for_1_symbol = 4
-Books_in_disk = int(space_disk_in_Bytes // (pages_in_book * lines_in_book * symbols_in_line * Bytes_for_1_symbol))
+bytes_for_1_symbol = 4
+size_of_one_book = pages_in_book * lines_in_book * symbols_in_line * bytes_for_1_symbol
+books_in_disk = int(space_disk_in_bytes // size_of_one_book)
 
-print("Количество книг, помещающихся на дискету:", Books_in_disk)
+print("Количество книг, помещающихся на дискету:", books_in_disk)
